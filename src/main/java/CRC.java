f8e2dde00e8ed98e37da6566e5e240bb3cdbacb2
acisliver <7273322@naver.com>
2021-12-06 22:30:53 +0900

    Checksum format

--- src/main/java/CRC.java
+++ src/main/java/CRC.java
@@ -1,16 +1,28 @@
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.zip.Adler32;
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
 public class CRC {
     public static void main(String[] args) {
 
-        byte[][] test = {{1, 0, 0, 1}, {1, 0, 1, 1,}, {0, 0, 1, 0}, {0, 1, 0, 1}, {0, 1, 1, 0}};
+        // byte[][] test = {{1, 0, 0, 1}, {1, 0, 1, 1,}, {0, 0, 1, 0}, {0, 1, 0, 1}, {0, 1, 1, 0}};
+        String [] inputs = {"Computer", "Network", "Ko", "Daeeun", "2017108246"};
 
-        System.out.println(getCRC32(test[0]));
+        byte bytes[] = inputs[0].getBytes();
+
+        System.out.println(getCRC32(bytes));
     }
 
+    // Checksum
+    public static long getChecksum(Checksum checksum){
+
+        return checksum.getValue();
+    }
+
+    // CRC-32
     public static long getCRC32(byte[] bytes) {
 
         Checksum crc = new CRC32();
@@ -19,4 +31,36 @@ public class CRC {
         crc.reset();
         return calculated;
     }
+
+    // Internet Checksum
+    public static long getInternetChecksum(byte[] bytes){
+
+
+        return 0;
+    }
+
+    // Fletcher-16 Checksum
+    public static long getFletcherChecksum(byte[] bytes){
+
+        short sum1 = 0;
+        short sum2 = 0;
+        short modulus = 255;
+
+        for (int i = 0; i < bytes.length; i++){
+            sum1 = (short) ((sum1 + bytes[i]) % modulus);
+            sum2 = (short) ((sum2 + sum1) % modulus);
+        }
+        return (short) ((sum2 << 8) | sum1);
+    }
+
+    // Adler Checksum
+    public static long getAdlerChecksum(byte[] bytes){
+
+        Checksum checksum = new Adler32();
+
+        checksum.update(bytes, 0, bytes.length);
+
+        return checksum.getValue();
+    }
+
 }
